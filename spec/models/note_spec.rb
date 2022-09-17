edc993c64681dff52690a555f9405f7a4cc389f2
talasago <42575556+talasago@users.noreply.github.com>
2022-09-17 12:38:12 +0900

    test: Add class-method-test for note

---
+++ spec/models/note_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe Note, type: :model do
+  # 検索結果を検証するスペック
+
+  # 検索文字列に一致するメモを返すこと
+  it "returns notes that match the search term" do
+    user = User.create(
+      first_name: 'Joe',
+      last_name: 'Tester',
+      email: 'joetester@example.com',
+      password: 'password'
+    )
+    project = user.projects.create(
+      name: 'Test Project'
+    )
+
+    note1 = project.notes.create(
+      message: 'This is the first note.',
+      user: user
+    )
+    note2 = project.notes.create(
+      message: 'This is the second note.',
+      user: user
+    )
+    note3 = project.notes.create(
+      message: 'First, preheat the oven.',
+      user: user
+    )
+
+    expect(Note.search('first')).to include(note1, note3)
+    expect(Note.search('first')).to_not include(note2)
+    expect(Note.search('second')).to include(note2)
+  end
+
+  # 検索結果が1件も見つからなければ空のコレクションを返すこと
+  it 'returns an empty collection when no results are found' do
+    user = User.create(
+      first_name: 'Joe',
+      last_name: 'Tester',
+      email: 'joetester@example.com',
+      password: 'password'
+    )
+    project = user.projects.create(
+      name: 'Test Project'
+    )
+
+    project.notes.create(
+      message: 'This is the first note.',
+      user: user
+    )
+    expect(Note.search('message')).to be_empty
+  end
+end
